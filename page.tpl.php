287121a7dfbbe5aa63bbfa7834c0eabd5ee040f7
Rainer Halbmann <rhalbmann@gmail.com>
2013-10-02 19:01:40 +0200

    Marie Korrekturen 1

--- page.tpl.php
+++ page.tpl.php
@@ -82,3 +82,18 @@
     </div>
   <?php endif; ?>
 </div><!-- /page -->
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://inglist.de/piwik/";
+    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+    _paq.push(["setSiteId", "33"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
